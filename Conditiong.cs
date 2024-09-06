2157a229be72634b21ee06d311afb37377f36437
Elina Prykhodko <eprykhodko@mint-innovations.com>
2024-09-06 18:05:03 +0300

    Add conditiong for a car

--- Conditiong.cs
+++ Conditiong.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DecoratorPattern
 {
-    internal class Conditiong
+    public class Conditiong : CarDecorator
     {
+        public Conditiong(ICar car) : base(car) { }
+
+        public override string GetDescription()
+        {
+            return _car.GetDescription();
+        }
     }
 }
