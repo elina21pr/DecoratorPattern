ce4b20cfd45292189472108876e815e12ff76bda
Elina Prykhodko <eprykhodko@mint-innovations.com>
2024-09-06 18:18:27 +0300

    Add options HeatedSeats

---
+++ Options/HeatedSeats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Options
+{
+    public class HeatedSeats : CarDecorator
+    {
+        public HeatedSeats(ICar car) : base(car) { }
+
+        public override string GetDescription()
+        {
+            return _car.GetDescription() + " Heated seats";
+        }
+    }
+}
