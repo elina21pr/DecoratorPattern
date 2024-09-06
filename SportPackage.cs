6debe5b02f811767512efee0b5b8ce69fb43f3b8
Elina Prykhodko <eprykhodko@mint-innovations.com>
2024-09-06 18:12:25 +0300

    Create SportPackage

---
+++ SportPackage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+    public class SportPackage : CarDecorator
+    {
+        public SportPackage(ICar car) : base(car) { }
+
+        public override int GetSpeed()
+        {
+            return _car.GetSpeed() + 60;
+        }
+    }
+}
--- obj/Debug/net8.0/DecoratorPattern.AssemblyInfo.cs
+++ obj/Debug/net8.0/DecoratorPattern.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecoratorPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2157a229be72634b21ee06d311afb37377f36437")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b24c66ec7115b3fac3d3d4a92de8f5fa1088217f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecoratorPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecoratorPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
