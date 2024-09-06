2016ba4fc48993d55475458bc7daba9bf993326f
Elina Prykhodko <eprykhodko@mint-innovations.com>
2024-09-06 17:58:46 +0300

    Create Class CarDecorator for adding functionality

--- obj/Debug/net8.0/DecoratorPattern.AssemblyInfo.cs
+++ obj/Debug/net8.0/DecoratorPattern.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecoratorPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba92dd8aaf7006971d5aebb2f5f3b52a3496a0ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7fc4def7adcb5f41ae51134beac815362973b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecoratorPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecoratorPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
