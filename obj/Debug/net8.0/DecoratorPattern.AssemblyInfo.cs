e6a05d182b80a92be6f214fcb151b39a8c63b3a9
Elina Prykhodko <eprykhodko@mint-innovations.com>
2024-09-09 11:27:30 +0300

    Add .gitignore file

--- obj/Debug/net8.0/DecoratorPattern.AssemblyInfo.cs
+++ obj/Debug/net8.0/DecoratorPattern.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecoratorPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a2670fb6c6286afc3f583e3f6f07daa7f3ba702")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edba07c96baaea24a5d960d1145c319c8abdfe50")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecoratorPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecoratorPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
